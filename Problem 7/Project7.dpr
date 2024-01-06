3de4eacb47b879f577321f0626e9a6dea3d95ca3
paul.mcgee.8@bigpond.com <Paul>
2024-01-06 17:08:18 +0800

    Directory Problem 7

---
+++ Problem 7/Project7.dpr
@@ -0,0 +1,43 @@
+{$APPTYPE CONSOLE}
+program Project7;
+{
+https://adventofcode.com/2022/day/7
+
+
+}
+
+uses
+  system.Classes,
+  generics.Collections,
+  U_Utils_Functional in '..\U_Utils_Functional.pas';
+
+begin
+
+       // Preliminaries
+
+   var sl := TStringList.create;
+       sl.LoadFromFile('..\..\Prob 7 Data.txt');
+
+
+       // Part 1
+
+
+       writeln;
+
+
+       // Part 2
+
+
+       writeln;
+
+
+
+       readln;
+
+   {   Part 1 answer :  _
+
+       Part 2 answer :  _  }
+end.
+
+
+
