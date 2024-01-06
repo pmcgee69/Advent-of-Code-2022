efd0690536baa5cf4def51674528b6902cc4066d
paul.mcgee.8@bigpond.com <Paul>
2024-01-06 14:00:30 +0800

    Directory Problem 6

---
+++ Problem 6/Project6.dpr
@@ -0,0 +1,53 @@
+{$APPTYPE CONSOLE}
+program Project6;
+{
+https://adventofcode.com/2022/day/6
+
+
+
+
+PART 1
+
+PART 2
+
+}
+
+uses
+  system.Classes,
+  system.SysUtils,
+  generics.Collections,
+  U_Utils_Functional in '..\U_Utils_Functional.pas';
+
+
+
+
+begin
+
+       // Preliminaries
+
+
+
+   var sl := TStringList.create;
+       sl.LoadFromFile('..\..\Prob 6 Data.txt');
+
+
+       // Part 1
+
+
+
+
+       // Part 2
+
+
+
+
+       readln;
+
+   {   Part 1 answer :
+
+       Part 2 answer :    }
+end.
+
+
+
+
