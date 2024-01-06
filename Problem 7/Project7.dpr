a992383726900cb5bb1bf6851bc316a0c2286f98
paul.mcgee.8@bigpond.com <Paul>
2024-01-06 23:34:52 +0800

    Begin Problem 7

--- Problem 7/Project7.dpr
+++ Problem 7/Project7.dpr
@@ -3,13 +3,120 @@ program Project7;
 {
 https://adventofcode.com/2022/day/7
 
+The filesystem consists of a tree of files (plain data) and directories (which can contain other directories or files).
+The outermost directory is called /.
 
+Within the terminal output, lines that begin with $ are commands you executed.
+
+  cd     means change directory. This changes which directory is the current directory, but the specific result depends on the argument:
+  cd x   moves in one level: it looks in the current directory for the directory named x and makes it the current directory.
+  cd ..  moves out one level: it finds the directory that contains the current directory, then makes that directory the current directory.
+  cd /   switches the current directory to the outermost directory, /.
+
+  ls     means list. It prints out all of the files and directories immediately contained by the current directory:
+         123 abc means that the current directory contains a file named abc with size 123.
+         dir xyz means that the current directory contains a directory named xyz.
+
+Given the commands and output in the example above, you can determine that the filesystem looks visually like this:
+
+- / (dir)
+     - a (dir)
+         - e (dir)
+             - i (file, size=584)
+         - f     (file, size=29116)
+         - g     (file, size=2557)
+         - h.lst (file, size=62596)
+     - b.txt (file, size=14848514)
+     - c.dat (file, size=8504156)
+     - d     (dir)
+         - j     (file, size=4060174)
+         - d.log (file, size=8033020)
+         - d.ext (file, size=5626152)
+         - k     (file, size=7214296)
+
+
+$ cd /
+$ ls
+  dir a
+  14848514 b.txt
+  8504156  c.dat
+  dir d
+
+$ cd a
+$ ls
+  dir e
+  29116    f
+  2557     g
+  62596    h.lst
+
+$ cd e
+$ ls
+  584      i
+
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+  4060174  j
+  8033020  d.log
+  5626152  d.ext
+  7214296  k
 }
 
 uses
   system.Classes,
+  system.SysUtils,
   generics.Collections,
   U_Utils_Functional in '..\U_Utils_Functional.pas';
+
+
+// Planning
+//
+//   data structure ...   Dict < string, record >   ...   Size.  List of (file, size) .  List of string.   Parent.
+//
+//   finite state machine :   cd  - add ?  move current
+//                        :   ls  - add dirs or files
+//
+
+type
+  Line_mode = (input, output);
+
+  TFile_tuple = record
+       filename : string;
+       filesize : cardinal;
+  end;
+
+  TDir_record = record
+       file_size : cardinal;
+       parent    : string;
+       files     : TList<TFile_tuple>;
+       subdirs   : TList<string>;
+  end;
+
+  TDirs_type = TDictionary< string, TDir_record >;
+
+
+       // functions
+
+procedure process_cd( var dirs:tdirs_type; cur_dir, s:string );
+begin
+
+end;
+
+
+procedure process_dir( var dirs:tdirs_type; cur_dir, s:string );
+begin
+
+end;
+
+
+procedure process_file( var dirs:tdirs_type; cur_dir, s:string );
+begin
+
+end;
+
+
+       // main
 
 begin
 
@@ -18,8 +125,31 @@ begin
    var sl := TStringList.create;
        sl.LoadFromFile('..\..\Prob 7 Data.txt');
 
-
        // Part 1
+
+   var dirs        := TDirs_type.Create;
+   var mode        := input;
+   var current_dir := '/';
+
+       for var s in sl do begin
+
+           var text := s.Split( [' '] );                       //  $ cd place   or   $ ls
+                                                               //  dir xyz      or   ddddd filename
+               if text[0]='$' then mode := input
+                              else mode := output;
+
+               case mode of
+                    input :  begin
+                                if text[1]='cd'  then  process_cd  ( dirs, current_dir, text[2] );
+                             end;
+
+                    output : begin
+                                if text[0]='dir' then  process_dir ( dirs, current_dir, text[1] )
+
+                                                 else  process_file( dirs, current_dir, text[1] );
+                             end;
+               end;
+       end;
 
 
        writeln;
@@ -39,5 +169,34 @@ begin
        Part 2 answer :  _  }
 end.
 
+  //TSub_dirs = TList<string>;
 
+{
+
+$ cd /
+$ ls
+dir hdwsmn
+dir lmlsvqsw
+dir rlfgcqz
+dir sjq
+dir tpnspw
+$ cd hdwsmn
+$ ls
+dir mrrqnc
+dir qst
+dir rlfgcqz
+$ cd mrrqnc
+$ ls
+227398 rwhw
+$ cd ..
+$ cd qst
+$ ls
+152795 bblss.hnl
+dir lvs
+$ cd lvs
+$ ls
+81813 jwvtjgjb.sss
+$ cd ..
+
+}
 
